0b754167a459cfe37c2fef5c68c670090ba9b473
Isaiah <ijandicos90@gmail.com>
2022-08-19 22:27:36 -0400

    Completed Assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,42 @@
+class SpicesController < ApplicationController
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def show
+        spice = Spice.find_by(id: params[:id])
+        render json: spice
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = Spice.find_by(id: params[:id])
+        if spice
+            spice.update(spice_params)
+            render json: spice
+        else
+        render json: {error: "Spice not found"}, status: :not_found
+        end
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        if spice
+            spice.destroy
+            head :no_content
+        else
+            render json: {error: "Spice not found"}, status: :not_found
+        end
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
